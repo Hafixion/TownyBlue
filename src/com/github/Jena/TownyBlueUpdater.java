0a826c2a107a8c9ca4592f922f729e0c59ffa42a
Shehab <shehabhafez3@gmail.com>
2020-11-09 23:15:41 +0200

    goodnight

--- src/com/github/Jena/TownyBlueUpdater.java
+++ src/com/github/Jena/TownyBlueUpdater.java
@@ -1,16 +1,23 @@
 package com.github.Jena;
 
 import com.palmergames.bukkit.towny.TownyAPI;
-import com.palmergames.bukkit.towny.exceptions.TownyException;
+import com.palmergames.bukkit.towny.exceptions.NotRegisteredException;
+import com.palmergames.bukkit.towny.object.Resident;
 import com.palmergames.bukkit.towny.object.Town;
+import com.palmergames.bukkit.towny.object.TownBlock;
 import com.palmergames.bukkit.towny.object.TownyWorld;
+import com.sun.deploy.util.StringUtils;
 import de.bluecolored.bluemap.api.BlueMapAPI;
 import de.bluecolored.bluemap.api.BlueMapMap;
 import de.bluecolored.bluemap.api.BlueMapWorld;
 import de.bluecolored.bluemap.api.marker.MarkerSet;
 import de.bluecolored.bluemap.api.marker.Shape;
+import de.bluecolored.bluemap.api.marker.ShapeMarker;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class TownyBlueUpdater {
 
@@ -22,19 +29,53 @@ public class TownyBlueUpdater {
                     TownyWorld townyWorld = TownyAPI.getInstance().getTownyWorld(world.getName());
 
                     for (Town town : townyWorld.getTowns().values()) {
-                        try {
-                            double xvalue = town.getHomeBlock().getCoord().getX() * 16;
-                            double zvalue = town.getHomeBlock().getCoord().getZ() * 16;
+                        for (TownBlock townBlock : town.getTownBlocks()) {
+                            double xvalue = townBlock.getCoord().getX() * 16;
+                            double zvalue = townBlock.getCoord().getZ() * 16;
 
                             Shape home = Shape.createRect(xvalue, zvalue, xvalue + 15, zvalue + 15);
-                            set.createShapeMarker(town.getName(), map, home, 64);
-                            set.setLabel(town.getName());
-                        } catch (TownyException e) {
-                            e.printStackTrace();
+                            ShapeMarker marker = set.createShapeMarker(town.getName(), map, home, TownyBlue.config.getInt("y-height"));
+                            marker.setLabel(getHTMLforTown(town));
                         }
                     }
                 }
             }
         }
+    }
+
+    @SuppressWarnings("ResultOfMethodCallIgnored")
+    public static String getHTMLforTown(Town town) {
+        String result = null;
+        // get the main html string
+        String Html = TownyBlue.config.getString("html");
+        String stringresidents = getResidents(town);
+
+        // placeholders
+        Html.replace("%name%", town.getName());
+        try {
+            Html.replace("%nation%", town.getNation().getName());
+        } catch (NotRegisteredException e) {
+            e.printStackTrace();
+            Html.replace("%nation%", "");
+        }
+        Html.replace("%mayor%", town.getMayor().getName());
+        Html.replace("%residents%", stringresidents);
+
+        if (Html != null) {result = Html;}
+
+        return result;
+    }
+
+    private static String getResidents(Town town) {
+        String result = null;
+        List<String> list = new LinkedList<>();
+
+        for (Resident resident : town.getResidents()) {
+            list.add(resident.getName());
+        }
+
+        if (list.toArray().length >= 1) {result = StringUtils.join(list, ", ");}
+        
+        return result;
     }
 }
