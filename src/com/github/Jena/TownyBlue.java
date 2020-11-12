851c46007d046f141a2a2a7a160c6b6e5a202223
Shehab <shehabhafez3@gmail.com>
2020-11-12 16:01:46 +0200

    Async

--- src/com/github/Jena/TownyBlue.java
+++ src/com/github/Jena/TownyBlue.java
@@ -62,7 +62,8 @@ public class TownyBlue extends JavaPlugin {
         }
 
         if (config.getBoolean("updating")) {
-            Bukkit.getScheduler().scheduleSyncRepeatingTask(plugin, TownyBlueUpdater.Update, 0, config.getLong("update") * 20);
+            //noinspection deprecation
+            Bukkit.getScheduler().scheduleAsyncRepeatingTask(plugin, TownyBlueUpdater.Update, 0, config.getLong("update") * 20);
         }
     }
 
